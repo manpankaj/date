a18afbf74ffd66484ef3d1511a4f3aa5a68072f5
manpankaj <manpankaj@gmail.com>
2021-02-20 15:57:02 +0530

    this is 8 commit

--- app/src/main/java/date/jalaunup/worker_changePActivity.java
+++ app/src/main/java/date/jalaunup/worker_changePActivity.java
@@ -1,4 +1,5 @@
 package date.jalaunup;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,7 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
 import androidx.appcompat.app.AppCompatActivity;
 public class worker_changePActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -15,10 +25,11 @@ public class worker_changePActivity extends AppCompatActivity {
     public static final String USERNAME = "username";
     public static final String EMAIL = "email";
     EditText ed_oldpassword,ed_password,ed_password1;
-    String str_oldpassword,str_password;
+    String str_oldpassword,str_password,str_mobile;
+    String url = "http://10.135.217.19:8080/date/worker_changeP.php";
     String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
     TextView username,email;
-    Button logout,back,changepassword;
+    Button logout,back;
     private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,5 +64,64 @@ public class worker_changePActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void changeP(View view) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please Wait..");
+
+        if(ed_oldpassword.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Old Password", Toast.LENGTH_SHORT).show();
+        }
+        else if(!ed_oldpassword.getText().toString().matches(passwordPattern)){
+            Toast.makeText(this, "Old Password must contain minimum 8 characters at least 1 Alphabet, 1 Number and 1 Special Character.", Toast.LENGTH_SHORT).show();
+        }
+        else if(ed_password.getText().toString().equals("")){
+            Toast.makeText(this, "Enter New Password", Toast.LENGTH_SHORT).show();
+        }
+        else if(!ed_password.getText().toString().matches(passwordPattern)){
+            Toast.makeText(this, "New Password must contain minimum 8 characters at least 1 Alphabet, 1 Number and 1 Special Character.", Toast.LENGTH_SHORT).show();
+        }
+        else if(ed_password1.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Confirm Password", Toast.LENGTH_SHORT).show();
+        }
+        else if(!ed_password1.getText().toString().equals (ed_password.getText().toString())){
+            Toast.makeText(this, "Password and Confirm Password Does not match", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            progressDialog.show();
+            str_mobile = sharedPreferences.getString(EMAIL, "");
+            str_oldpassword = ed_oldpassword.getText().toString().trim();
+            str_password = ed_password.getText().toString().trim();
+            String encrypt_oldpassword = password_encrypt.getSha256Hash(str_oldpassword);
+            String encrypt_password = password_encrypt.getSha256Hash(str_password);
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+                    ed_oldpassword.setText("");
+                    ed_password.setText("");
+                    ed_password1.setText("");
+                    Toast.makeText(worker_changePActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            },new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(worker_changePActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("mobile",str_mobile);
+                    params.put("oldpassword",encrypt_oldpassword);
+                    params.put("password",encrypt_password);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(worker_changePActivity.this);
+            requestQueue.add(request);
+        }
     }
 }
